8d000b5b944eb8e61838198b022bd6545bbc35fa
Jonas Bostoen <jonas.bostoen@hotmail.com>
2018-12-07 11:29:05 +0100

    fixed client

--- client.py
+++ client.py
@@ -1,23 +1,30 @@
 import socket
+import getpass
 
 def client():
+    source_port = 4444
+    source_host = "localhost"
     host = "127.0.0.1"
     port = 1234
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    client_socket.bind((source_host, source_port))
     client_socket.connect((host, port))
 
-    message = input("> ")
-    exit_code = "close"
+    exit_codes = ["wrong password.", "process completed."]
 
-    while message.lower().strip() != exit_code:
-        client_socket.send(message.encode())
+    while True:
         data = client_socket.recv(1024).decode()
+        print(data)
+        
+        if data.lower().strip() in exit_codes:
+            break
 
-        print("Server > " + data)
+        message = getpass.getpass("Password: ")
+        client_socket.send(message.encode())
 
-        message = input("> ")
-    client_socket.send(exit_code.encode())
+
     client_socket.close()
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-import subprocess as sub
+import subprocess as sp
 
 def server():
     # Define server
@@ -10,14 +10,26 @@ def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((host, port))
-
+    
+    # Listen for 1 connection at a time
     server_socket.listen(1)
     print("{} listening on port {}".format(host, port))
     conn, address = server_socket.accept()
+    
+    # Read whitelisted IPs and ports from file
+    ip_file = open("allowed_ips.txt", "r")
+    ip_list = [line[:-1] for line in ip_file]
+    print(ip_list) 
+
+    ip_file.close()
+    
+    port_file = open("allowed_ports.txt", "r")
+    port_list = [int(line[:-1]) for line in port_file]
+    print(port_list)
+
+    port_file.close()
 
     # Security checks, add allowed ip's and ports to lists
-    ip_list = ["127.0.0.1"]
-    port_list = [1234, 4444]
     print("Connection from {} on port {}".format(str(address[0]), str(address[1])))
 
     if str(address[0]) not in ip_list:
@@ -32,20 +44,22 @@ def server():
     password = "testpassword"
 
     while recv:
-        data = "password > "
+        data = "Please enter the password below.\n"
         conn.send(data.encode())
         recv = str(conn.recv(1024).decode())
         print("Client > " + recv)
         
         if recv.lower().strip() != password:
+            conn.send("Wrong password.".encode())
             conn.close()
             return "ERROR: Wrong password"
         
         elif recv.lower().strip() == password:
-            #output = str(sub.run(["ls"], capture_output=True).stdout) + "\n"
-            #print(sub.run(["ls"], capture_output=True).stdout)
+            #output = str(sp.run(["ls"], capture_output=True).stdout) + "\n"
+            #print(sp.run(["ls"], capture_output=True).stdout)
             #conn.send(output.encode())
-            sub.run(["/home/arched/Projects/RemoteExec/python/example_script.sh"])
+            sp.run(["/home/arched/Projects/RemoteExec/python/example_script.sh"])
+            conn.send("Process completed.".encode())
             server_socket.close()
             conn.close()
             return "Process Complete"
